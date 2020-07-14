f946543744ed62d4fd4aafc586f9ab2b4b45d297
Hugo Gameiro <hmgameiro@gmail.com>
2020-07-14 19:14:35 +0100

    remotable with synchro

--- app/models/concerns/remotable.rb
+++ app/models/concerns/remotable.rb
@@ -2,6 +2,7 @@
 
 module Remotable
   extend ActiveSupport::Concern
+  include RoutingHelper
 
   class_methods do
     def remotable_attachment(attachment_name, limit, suppress_errors: true)
@@ -20,8 +21,16 @@ module Remotable
 
         return if !%w(http https).include?(parsed_url.scheme) || parsed_url.host.blank? || (self[attribute_name] == url && send("#{attachment_name}_file_name").present?)
 
+        # If a file is likely to go through ffmpeg, try the synchronization mechanisms
+        needs_synchronization = (MediaAttachment::VIDEO_FILE_EXTENSIONS + ['.gif']).include?(File.extname(parsed_url.basename).downcase)
+        processed_url = RemoteSynchronizationManager.instance.get_processed_url(url) if needs_synchronization
+
+        if processed_url == RemoteSynchronizationManager::PROCESSING_VALUE
+          raise Mastodon::UnexpectedResponseError unless suppress_errors
+          return
+        end
         begin
-          Request.new(:get, url).perform do |response|
+          Request.new(:get, processed_url || url).perform do |response|
             raise Mastodon::UnexpectedResponseError, response unless (200...300).cover?(response.code)
 
             content_type = parse_content_type(response.headers.get('content-type').last)
@@ -43,11 +52,18 @@ module Remotable
           end
         rescue Mastodon::UnexpectedResponseError, HTTP::TimeoutError, HTTP::ConnectionError, OpenSSL::SSL::SSLError => e
           Rails.logger.debug "Error fetching remote #{attachment_name}: #{e}"
+          # We failed processing, release lock on processing media
+          RemoteSynchronizationManager.instance.set_processed_url(url, nil) if processed_url.nil? && needs_synchronization
           raise e unless suppress_errors
         rescue Paperclip::Errors::NotIdentifiedByImageMagickError, Addressable::URI::InvalidURIError, Mastodon::HostValidationError, Mastodon::LengthValidationError, Paperclip::Error, Mastodon::DimensionsValidationError => e
           Rails.logger.debug "Error fetching remote #{attachment_name}: #{e}"
           nil
         end
+
+        # File successfuly processed, get its url
+        RemoteSynchronizationManager.instance.set_processed_url(url, full_asset_url(public_send("#{attachment_name}").url(:original))) if processed_url.nil? && needs_synchronization
+
+        nil
       end
 
       define_method alt_method_name do
