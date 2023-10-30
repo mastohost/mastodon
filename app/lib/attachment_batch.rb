6b270f403b4906d064fef71e25459105ee3078a7
Hugo Gameiro <email@hugogameiro.com>
2023-10-30 10:24:26 +0000

    revert throttling

--- app/lib/attachment_batch.rb
+++ app/lib/attachment_batch.rb
@@ -75,9 +75,7 @@ class AttachmentBatch
             end
           when :fog
             logger.debug { "Deleting #{attachment.path(style)}" }
-            # throttle to avoid 429 Too Many Requests - OVH 60 auth tokens per minute
-            sleep(0.5)
-              
+
             begin
               attachment.send(:directory).files.new(key: attachment.path(style)).destroy
             rescue Fog::Storage::OpenStack::NotFound
