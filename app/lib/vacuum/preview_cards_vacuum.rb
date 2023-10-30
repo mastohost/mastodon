bb29d354b0e6d3db52da3b8f08a90a7d920208cd
Hugo Gameiro <email@hugogameiro.com>
2023-10-30 11:57:37 +0000

    remove throttle

--- app/lib/vacuum/preview_cards_vacuum.rb
+++ app/lib/vacuum/preview_cards_vacuum.rb
@@ -5,7 +5,6 @@ class Vacuum::PreviewCardsVacuum
 
   def initialize(retention_period)
     @retention_period = retention_period
-    @storage_mode     = Paperclip::Attachment.default_options[:storage]
   end
 
   def perform
@@ -18,10 +17,6 @@ class Vacuum::PreviewCardsVacuum
     preview_cards_past_retention_period.find_each do |preview_card|
       preview_card.image.destroy
       preview_card.save
-      if @storage_mode == :fog
-        # throttle to avoid 429 Too Many Requests - OVH 60 auth tokens per minute
-        sleep(1.5)
-      end
     end
   end
 
